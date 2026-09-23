ebb7234438dbf79934952fa863d3b668c9035e2d
kaarr <rainbowcotton.candyy@gmail.com>
2026-09-24 00:54:02 +0700

    feat(w02): implement responsive academic dashboard with layoutbuilder and m3 theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'modul_01/profile_screen.dart';
-import 'modul_02/academic_dashboard_screen.dart';
+import 'modul_02/academic_dashboard_screen.txt';
 import 'modul_03/modul_03_app.dart';
 import 'modul_04/modul_04_app.dart';
 
